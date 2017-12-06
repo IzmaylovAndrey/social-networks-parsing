31e78e5f771087e6c14a7e534966cb9868e64f7f
Margarita <margo.tuleninova@gmail.com>
2017-12-06 10:39:49 +0300

    pg to docker && some refactor

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"./models"
+	"github.com/IzmaylovAndrey/social-networks-parsing/models"
 	//"time"
 	//"fmt"
-	//"./utils"
+	//"github.com/IzmaylovAndrey/social-networks-parsing/utils"
 )
 
 func main() {
@@ -12,14 +12,15 @@ func main() {
 	//models.Migrate(*db)
 	//user := models.Users {ID: "0b454430-5292-449e-9c84-2ba3e6e6578e", Login: "maggy93@mail.ru", PasswordHash: "1234", Salt: "3456", CreatedAt: time.Now()}
 	//models.Create(user, *db)
+	//fmt.Printf("%s", user.Login)
 	//models.GetByID("0d29fcb9-1129-4b23-9084-763012be284a", *db)
 	//models.GetByLogin("helpik94@ysndex.ru", *db)
 	//users, _ := models.GetAll(*db)
 	//users, _ := models.GetAllOrderbyCreationDesc(*db)
-	//fmt.Printf("%s", (*users)[0].Login)
+	//fmt.Printf("%s", users[0].ID)
 	//account := models.Accounts{ID: "86f18aaf-3340-40ea-9b04-769c65808d11", UserID: "0b454430-5292-449e-9c84-2ba3e6e6578e", SocialNetwork: "vk", Data: "{}"}
 	//models.Add(account, *db)
 	//accounts, _ := models.GetAccountsByUserID("0b454430-5292-449e-9c84-2ba3e6e6578e", *db)
-	//utils.SendToTelegram("maggy93@mail.ru", *accounts)
+	//utils.SendToTelegram("maggy93@mail.ru", accounts)
 	models.CloseConnection(*db)
 }
--- models/users.go
+++ models/users.go
@@ -9,6 +9,7 @@ import (
 type Users struct {
 	ID 				string `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
 	Login 			string
+	Name			string
 	PasswordHash 	string
 	Salt 			string
 	CreatedAt 		time.Time
@@ -42,52 +43,62 @@ func GetByLogin (login string, db gorm.DB) (*Users, error) {
 	return &user, nil
 }
 
-func GetAll (db gorm.DB) (*[]Users, error) {
+func GetByName (name string, db gorm.DB) (*Users, error) {
+	var user Users
+	if err := db.Where("name = ?", name).First(&user).Error; err != nil {
+		fmt.Printf("User getting by login error: %s", err)
+		return nil, err
+	}
+	fmt.Printf("%s", user.Name)
+	return &user, nil
+}
+
+func GetAll (db gorm.DB) ([]Users, error) {
 	var users []Users
 	if err := db.Find(&users).Error; err != nil {
 		fmt.Printf("User list getting error: %s", err)
 		return nil, err
 	}
 	fmt.Printf("%s", users[0].Login)
-	return &users, nil
+	return users, nil
 }
 
-func GetAllOrderbyID (db gorm.DB) (*[]Users, error) {
+func GetAllOrderbyID (db gorm.DB) ([]Users, error) {
 	var users []Users
 	if err := db.Order("id").Find(&users).Error; err != nil {
 		fmt.Printf("User list ordered by id getting error: %s", err)
 		return nil, err
 	}
 	fmt.Printf("%s", users[0].Login)
-	return &users, nil
+	return users, nil
 }
 
-func GetAllOrderbyLogin (db gorm.DB) (*[]Users, error) {
+func GetAllOrderbyLogin (db gorm.DB) ([]Users, error) {
 	var users []Users
 	if err := db.Order("login").Find(&users).Error; err != nil {
 		fmt.Printf("User list ordered by login getting error: %s", err)
 		return nil, err
 	}
 	fmt.Printf("%s", users[0].Login)
-	return &users, nil
+	return users, nil
 }
 
-func GetAllOrderbyCreation (db gorm.DB) (*[]Users, error) {
+func GetAllOrderbyCreation (db gorm.DB) ([]Users, error) {
 	var users []Users
 	if err := db.Order("created_at").Find(&users).Error; err != nil {
 		fmt.Printf("User list ordered by creation date getting error: %s", err)
 		return nil, err
 	}
 	fmt.Printf("%s", users[0].Login)
-	return &users, nil
+	return users, nil
 }
 
-func GetAllOrderbyCreationDesc (db gorm.DB) (*[]Users, error) {
+func GetAllOrderbyCreationDesc (db gorm.DB) ([]Users, error) {
 	var users []Users
 	if err := db.Order("created_at desc").Find(&users).Error; err != nil {
 		fmt.Printf("User list ordered by creation date desc getting error: %s", err)
 		return nil, err
 	}
 	fmt.Printf("%s", users[0].Login)
-	return &users, nil
+	return users, nil
 }
